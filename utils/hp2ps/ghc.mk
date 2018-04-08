6bb28ac507d42602a3346e0b18c110810be1763e
Debian Haskell Group <pkg-haskell-maintainers@lists.alioth.debian.org>
2018-04-08 21:24:00 +0100

    build-unlit-and-hp2ps-twice
    Gbp-Pq: Name build-unlit-and-hp2ps-twice.patch

--- utils/hp2ps/ghc.mk
+++ utils/hp2ps/ghc.mk
@@ -17,7 +17,7 @@ utils/hp2ps_dist_C_SRCS          = AreaBelow.c Curves.c Error.c Main.c \
                                    Utilities.c
 utils/hp2ps_dist_EXTRA_LIBRARIES = m
 utils/hp2ps_dist_PROGNAME        = hp2ps
-utils/hp2ps_dist_INSTALL         = YES
+utils/hp2ps_dist_INSTALL         = NO
 utils/hp2ps_dist_INSTALL_INPLACE = YES
 utils/hp2ps_dist_SHELL_WRAPPER              = YES
 utils/hp2ps_dist_INSTALL_SHELL_WRAPPER_NAME = hp2ps
@@ -25,3 +25,17 @@ utils/hp2ps_dist_INSTALL_SHELL_WRAPPER_NAME = hp2ps
 utils/hp2ps_CC_OPTS += $(addprefix -I,$(GHC_INCLUDE_DIRS))
 
 $(eval $(call build-prog,utils/hp2ps,dist,0))
+
+utils/hp2ps_dist-install_C_SRCS  = AreaBelow.c Curves.c Error.c Main.c \
+                                   Reorder.c TopTwenty.c AuxFile.c Deviation.c \
+                                   HpFile.c Marks.c Scale.c TraceElement.c \
+                                   Axes.c Dimensions.c Key.c PsFile.c Shade.c \
+                                   Utilities.c
+utils/hp2ps_dist-install_EXTRA_LIBRARIES = m
+utils/hp2ps_dist-install_PROGNAME = hp2ps
+utils/hp2ps_dist-install_TOPDIR  = YES
+utils/hp2ps_dist-install_INSTALL = YES
+utils/hp2ps_dist-install_INSTALL_INPLACE = NO
+
+# See Note [Why build certain utils twice?].
+$(eval $(call build-prog,utils/hp2ps,dist-install,1))
