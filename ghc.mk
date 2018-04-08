dea7670a472ae86ac3a1b96bd637e1a752486a50
Debian Haskell Group <pkg-haskell-maintainers@lists.alioth.debian.org>
2018-04-08 21:24:00 +0100

    use-debian-gen_contents_index
    Gbp-Pq: Name use-debian-gen_contents_index

--- ghc.mk
+++ ghc.mk
@@ -829,7 +829,6 @@ endif
 # Build the Haddock contents and index
 ifeq "$(HADDOCK_DOCS)" "YES"
 libraries/dist-haddock/index.html: $(haddock_INPLACE) $(ALL_HADDOCK_FILES)
-	cd libraries && sh gen_contents_index --intree
 ifeq "$(phase)" "final"
 $(eval $(call all-target,library_doc_index,libraries/dist-haddock/index.html))
 endif
@@ -950,12 +949,8 @@ endif
 	$(INSTALL_DIR) "$(DESTDIR)$(docdir)/html"
 	$(INSTALL_DOC) $(INSTALL_OPTS) docs/index.html "$(DESTDIR)$(docdir)/html"
 ifneq "$(INSTALL_LIBRARY_DOCS)" ""
-	$(INSTALL_DIR) "$(DESTDIR)$(docdir)/html/libraries"
-	for i in $(INSTALL_LIBRARY_DOCS); do \
-		$(INSTALL_DOC) $(INSTALL_OPTS) $$i "$(DESTDIR)$(docdir)/html/libraries/"; \
-	done
+	mkdir -p $(DESTDIR)$(docdir)/html/libraries/
 	$(INSTALL_DATA) $(INSTALL_OPTS) libraries/prologue.txt "$(DESTDIR)$(docdir)/html/libraries/"
-	$(INSTALL_SCRIPT) $(INSTALL_OPTS) libraries/gen_contents_index "$(DESTDIR)$(docdir)/html/libraries/"
 endif
 ifneq "$(INSTALL_HTML_DOC_DIRS)" ""
 	for i in $(INSTALL_HTML_DOC_DIRS); do \
@@ -1067,7 +1062,6 @@ $(eval $(call bindist-list,.,\
     mk/project.mk \
     mk/install.mk.in \
     bindist.mk \
-    libraries/gen_contents_index \
     libraries/prologue.txt \
     $(wildcard libraries/dph/LICENSE \
                libraries/dph/ghc-packages \
