b7de242492e2eca821bc30040589ada1300bc5e5
Debian Haskell Group <pkg-haskell-maintainers@lists.alioth.debian.org>
2024-11-06 00:21:23 +0100

    hurd-cabal-supportrpaths
    https://github.com/haskell/cabal/pull/9441
    Debian-Bug: https://bugs.debian.org/1056305
    
    
    Gbp-Pq: Name hurd-cabal-supportrpaths

--- libraries/Cabal/Cabal/src/Distribution/Simple/GHCJS.hs
+++ libraries/Cabal/Cabal/src/Distribution/Simple/GHCJS.hs
@@ -1537,7 +1537,7 @@ getRPaths lbi clbi | supportRPaths hostOS = do
     supportRPaths Android     = False
     supportRPaths Ghcjs       = False
     supportRPaths Wasi        = False
-    supportRPaths Hurd        = False
+    supportRPaths Hurd        = True
     supportRPaths (OtherOS _) = False
     -- Do _not_ add a default case so that we get a warning here when a new OS
     -- is added.
